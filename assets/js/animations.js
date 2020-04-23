b475fcac08e3e79e8d519b48879513572ebacf50
Fabio Mauri <cripty2001@users.noreply.github.com>
2020-04-23 10:53:19 +0200

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -5,7 +5,15 @@ function parseAnimation(an) {
 	const animations = [];
 
 	for (let i = 0; i < Math.floor(pos.height); i++) {
-		animations[i] = an.style(i / pos.height);
+		animations[i] = an.style({
+			pos: {
+				percent: i / pos.height,	// %-scroll position in container
+				absolute: i			// px-scoll position in container
+			},
+			containerDimension: {
+				height: pos.height,		// Height (in px) of the container
+				width: pos.wid
+		});
 	}
 
 	return animations
@@ -19,18 +27,19 @@ function parseAnimation(an) {
 function buildAnimationList() {    
     window.animations = [];
     
-	[{
+	[
+		{
+			container: 'sec1',
+			selectors: ['#sec1 > .titleContent'],
+			style: ({pos, containerDimension, windowDimension}) => ({
+				transform: `scale(${Math.min(1, (1 - pos))})`,
+			})
+		},
+		{
 			container: 'sec2',
 			selectors: ['#sec2 > .staticImage'],
 			style: pos => ({
 				filter: `brightness(${Math.max(0, pos)})`
-			})
-		},
-		{
-			container: 'sec1',
-			selectors: ['#sec1 > .titleContent'],
-			style: pos => ({
-				transform: `scale(${Math.min(1, (1 - pos))})`,
 			})
 		},
 	]
@@ -42,6 +51,22 @@ function buildAnimationList() {
 			}
 			window.animations[item.pos].push(item);
 		});
+}
+
+function getVh() {
+	return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+}
+
+function getVw(v) {
+	return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+}
+
+function getVmin(v) {
+	return Math.min(vh(), vw());
+}
+
+function vmax(v) {
+	return Math.max(vh(), vw());
 }
 
 function subscribeAnimationFrame() {
